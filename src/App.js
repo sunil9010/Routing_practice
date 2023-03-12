ffdc7a34589c8fc980a05efc3cfb3ce866280c1c
sunil9010 <tsn9010@gmail.com>
2023-03-12 04:48:26 +0000

    Routing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Switch, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import About from './components/About'
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <Header />
+      <div className="app-body">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      className="contact-img "
+      alt="contact"
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+    />
+    <h1 className="contact-heading">Contact</h1>
+  </div>
+)
+export default Contact
